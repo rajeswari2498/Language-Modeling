9346ed92b839cee2c154703ba20b1eca4a2ac0bd
rajeswari2498 <rajeswariyesasvini@msitprogram.net>
2021-11-23 11:14:50 +0530

    completed the installation of matplotlib and numpy

--- language.py
+++ language.py
@@ -409,15 +409,18 @@ if __name__ == "__main__":
     test.testGenerateTextFromUnigrams()
     test.testGenerateTextFromBigrams()
 #     ## Uncomment these for Week 2 ##
-# """
-#     print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
-#     test.week2Tests()
-#     print("\n" + "#"*15 + " WEEK 2 OUTPUT " + "#" * 15 + "\n")
-#     test.runWeek2()
-# """
 
-#     ## Uncomment these for Week 3 ##
-# """
-#     print("\n" + "#"*15 + " WEEK 3 OUTPUT " + "#" * 15 + "\n")
-#     test.runWeek3()
-# """
+    print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
+    test.week2Tests()
+    print("\n" + "#"*15 + " WEEK 2 OUTPUT " + "#" * 15 + "\n")
+    test.runWeek2()
+
+
+
+
+
+
+
+    # ## Uncomment these for Week 3 ##
+    # print("\n" + "#"*15 + " WEEK 3 OUTPUT " + "#" * 15 + "\n")
+    # test.runWeek3()
